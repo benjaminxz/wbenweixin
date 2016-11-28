741e1aa6f20eca3fa6355c808f68cda9125e53ca
benjamin <novabarry>
2016-11-28 17:52:07 +0800

    Kbw-6 删除list中的数据 动态删除前台的数据

--- pages/deleteFromList/deleteFromList.js
+++ pages/deleteFromList/deleteFromList.js
@@ -3,7 +3,34 @@
 Page({
   data: {
     logs: [],
-    listDate:[]
+    listDate: [],
+  },
+  deleteItem: function (e) {
+    var that = this;
+    wx.request({
+      url: 'http://www.bjmwebapi.com/api/object/' + e.target.dataset.key,
+      data: {},
+      method: 'DELETE', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        console.log(res.data)
+
+        var listTemp = that.data.listDate
+        //    listTemp.push({apiCrudID:333,apiName:222})/////
+        var index = listTemp.indexOf(e.target.dataset.key);
+        listTemp.splice(index, 1);
+        that.setData({
+          listDate: listTemp
+        });
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
   },
   onLoad: function () {
     var that = this;
