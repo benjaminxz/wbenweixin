5ecf406a1691eac8c17b37879d6ef8dc90d20285
benjamin <novabarry>
2016-12-08 11:24:24 +0800

    Kbw-22 商家搜索与进入

--- pages/searchBusiness/searchBusiness.js
+++ pages/searchBusiness/searchBusiness.js
@@ -1,23 +1,46 @@
 // pages/searchBusiness/searchBusiness.js
 Page({
- data: {
-    logs: [],
-    oneDate: "",
-    idKey:"",
-    businessNameKey:""
+  data: {
+   
+   
+    idKey: "",
+    businessNameKey: "",
+    businessListData:"",
   },
   onLoad: function () {
     var that = this;
+    
+  },
+
+
+  idInputEvent: function (e) {
+    this.setData({ idKey: e.detail.value });
+  },
+
+  idClickEvent: function (e) {
+
+    wx.navigateTo({
+      url: '../productList/productList?id=' + this.data.businessNameKey/////////
+    });
+  },
+
+  businessNameInputEvent: function (e) {
+    this.setData({ businessNameKey: e.detail.value });
+  },
+
+  businessNameClickEvent: function (e) {
+
+    var that = this;
     wx.request({
-      url: 'http://www.bjmwebapi.com/api/yys',
-      data: {},
+      url: 'http://www.bjmwebapi.com/api/BusinessSearch',
+      data: { searchBusinessName:that.data.businessNameKey },
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       // header: {}, // 设置请求的 header
       success: function (res) {
         console.log(res.data)
         // success
         that.setData({
-          oneDate: res.data
+          businessListData: res.data
         });
       },
       fail: function () {
@@ -27,41 +50,25 @@ Page({
         // complete
       }
     })
+
   },
-
-
-  idInputEvent: function (e) {
-    this.setData({ businessNameKey: e.detail.value });
-  },
-
-  idClickEvent: function (e) {
-    
+  toProductPage: function (e) {
+    var bid = e.currentTarget.dataset.bid; //图书id [data-bid]
     wx.navigateTo({
-      url: '../productList/productList?id=' + this.data.businessNameKey/////////
+      url: '../productList/productList?id=' + bid
     });
   },
 
-    businessNameInputEvent: function (e) {
-    this.setData({ idKey: e.detail.value });
-  },
-
-  businessNameClickEvent: function (e) {
-    
-    wx.navigateTo({
-      url: '../productList/productList?id=' + this.data.idKey
-    });
-  },
-
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
